eb794e79415af3dd68228a803b059ba481e0f93b
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2024-01-05 23:17:13 +0500

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var p = new Person
+{
+    Name = "Otabek",
+    Age = 21
+};
+
+void PassReferenceByRef(ref Person p) 
+{
+    p.Name = "Og'abek";
+    p = new Person {Name="Anvar", Age = 23};
+}
+
+Console.WriteLine(p);
+PassReferenceByRef(ref p);
+Console.WriteLine(p);
